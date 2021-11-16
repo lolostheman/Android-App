ea169f8679604847fe2b746b431523dbe1bd3990
Ramon Veal <vealrt@vcu.edu>
2021-11-16 10:51:30 -0500

    Button to uncheck allitems

--- app/src/main/java/com/example/cmscapp/MainActivity.java
+++ app/src/main/java/com/example/cmscapp/MainActivity.java
@@ -69,4 +69,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void uncheckBoxes(View view) {
+        // cycle through the checkedIngredients list(interface?)
+    }
 }
